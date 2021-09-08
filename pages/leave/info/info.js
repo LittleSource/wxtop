0c8d9df4f92eb23df697653f79e48b80c5befeeb
LittleSource <444168950@qq.com>
2021-09-08 12:05:07 +0800

    reapir page error

--- pages/leave/info/info.js
+++ pages/leave/info/info.js
@@ -22,7 +22,6 @@ Page({
         this.setData({
             userInfo: app.globalData.userInfo
         })
-        console.log(userInfo);
         var nowdata = date.formatDateTime((new Date()).getTime() / 1000)
         var todayDate_ = nowdata[1] + '/' + nowdata[2]
         var number_ = "" + nowdata[0] + nowdata[1] + nowdata[2] + nowdata[3] + nowdata[4] + nowdata[5]
@@ -30,13 +29,11 @@ Page({
         eventChannel.on('message', function (data) {
             data.startDate = data.startDate.replace(/-/g, '/');
             data.endDate = data.endDate.replace(/-/g, '/');
-            var bottomDate_ = data.startDate.substr(5, 5);
             _self.setData({
-                userInfo: app.globalData.userInfo,
                 data: data,
                 number: number_,
                 todayDate: todayDate_,
-                bottomDate: bottomDate_
+                bottomDate: data.startDate.substr(5, 5)
             })
         })
     },
